3c3e630fe3fc5dcf7e3a1818eb0ce7882a35dcdc
embulldogs99 <embulldogs99@yahoo.com>
2018-02-16 19:20:28 -0800

    simplier

--- main.go
+++ main.go
@@ -17,10 +17,8 @@ func main() {
 
   http.Handle("/favicon/", http.StripPrefix("/favicon/", http.FileServer(http.Dir("./favicon"))))
   http.Handle("/pics/", http.StripPrefix("/pics/", http.FileServer(http.Dir("./pics"))))
-	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+  http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
   http.HandleFunc("/", serve)
-  http.HandleFunc("/about", serveabout)
-  http.HandleFunc("/contact", servecontact)
   log.Fatal(s.ListenAndServe())
 }
 
@@ -30,14 +28,3 @@ func serve(w http.ResponseWriter, r *http.Request){
   tpl.Execute(w, nil)
 }
 
-func serveabout(w http.ResponseWriter, r *http.Request){
-  var tpl *template.Template
-  tpl = template.Must(template.ParseFiles("about.gohtml","css/main.css","css/mcleod-reset.css" ))
-  tpl.Execute(w, nil)
-}
-
-func servecontact(w http.ResponseWriter, r *http.Request){
-  var tpl *template.Template
-  tpl = template.Must(template.ParseFiles("contact.gohtml","css/main.css","css/mcleod-reset.css" ))
-  tpl.Execute(w, nil)
-}
