db2ea7740008aa928da954ab44ba1dba5c69ec48
embulldogs99 <embulldogs99@yahoo.com>
2018-02-17 17:26:49 -0800

    automated posting

---
+++ localmain.go
@@ -0,0 +1,44 @@
+package main
+
+import(
+  "net/http"
+  	"html/template"
+    "log"
+)
+
+
+func main() {
+
+  s := &http.Server{
+
+    Addr:    ":8080",
+    Handler: nil,
+  }
+
+  http.Handle("/favicon/", http.StripPrefix("/favicon/", http.FileServer(http.Dir("./favicon"))))
+  http.Handle("/pics/", http.StripPrefix("/pics/", http.FileServer(http.Dir("./pics"))))
+  http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+  http.Handle("/svg/", http.StripPrefix("/svg/", http.FileServer(http.Dir("./svg"))))
+  http.HandleFunc("/", serve)
+  http.HandleFunc("/verify", serveverify)
+  http.HandleFunc("/weeklyregister", weeklyregister)
+  log.Fatal(s.ListenAndServe())
+}
+
+func serve(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("main.gohtml","css/main.css","css/mcleod-reset.css"))
+  tpl.Execute(w, nil)
+}
+
+func serveverify(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("verification.gohtml","css/main.css","css/mcleod-reset.css",))
+  tpl.Execute(w, nil)
+}
+
+func weeklyregister(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("tregistration.gohtml","css/main.css","css/mcleod-reset.css",))
+  tpl.Execute(w, nil)
+}
