2877e9442db7a6a5843e67e752b092192fa69fd4
embulldogs99 <embulldogs99@yahoo.com>
2018-02-19 14:40:41 -0800

    automated posting

--- maintest.go
+++ maintest.go
@@ -136,7 +136,7 @@ func profile(w http.ResponseWriter, r *http.Request){
       log.Fatal(err)
     default:
       var tpl *template.Template
-      tpl = template.New("name").Parse("profile.gohtml","css/main.css","css/mcleod-reset.css")
+      tpl, _ = template.New("name").Parse("profile.gohtml","css/main.css","css/mcleod-reset.css")
       tpl.Execute(w,data)
       fmt.Println(email+"has logged in")
 
