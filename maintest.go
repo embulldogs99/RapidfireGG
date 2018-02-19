5736ed24bc4e5430a13c9b8057fac9e1cba494df
embulldogs99 <embulldogs99@yahoo.com>
2018-02-18 21:34:07 -0800

    automated posting

---
+++ maintest.go
@@ -0,0 +1,128 @@
+package main
+
+import(
+  "net/http"
+  	"html/template"
+    "log"
+    "fmt"
+    "io/ioutil"
+    "database/sql"
+_ "github.com/lib/pq"
+
+
+)
+
+
+func main() {
+
+  s := &http.Server{
+
+    Addr:    ":8080",
+    Handler: nil,
+  }
+
+  http.Handle("/favicon/", http.StripPrefix("/favicon/", http.FileServer(http.Dir("./favicon"))))
+  http.Handle("/pics/", http.StripPrefix("/pics/", http.FileServer(http.Dir("./pics"))))
+  http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("./css"))))
+  http.Handle("/svg/", http.StripPrefix("/svg/", http.FileServer(http.Dir("./svg"))))
+  http.HandleFunc("/", serve)
+  http.HandleFunc("/verify", verify)
+  http.HandleFunc("/weeklyregister", weeklyregister)
+  http.HandleFunc("/waitingregister", waitingregister)
+  log.Fatal(s.ListenAndServe())
+
+
+
+
+/*type user struct {
+	email string
+	pass  string
+	ppal  bool
+	wins  int
+  losses int
+  heat int
+  refers int
+  memberflag int
+  credits float64
+  gread int
+}*/
+
+/*var dbs = map[string]string{} //session id, stores userids*/
+
+
+}
+
+
+func dbupost(e string,p string,pp bool,m bool) {
+  const (
+    host     = "localhost"
+    port     = 5432
+    user     = "postgres"
+    password = "postgres"
+    dbname   = "rfgg"
+  )
+    psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+      "password=%s dbname=%s sslmode=disable",
+      host, port, user, password, dbname)
+	//opens conncetion to db for use
+	dbusers, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Unable to connect to the database")
+	}
+
+
+  sqlStatement := `
+INSERT INTO rfgg.members (email, pass, ppal, memberflag)
+VALUES ($1, $2, $3, $4)`
+_, err = dbusers.Exec(sqlStatement, e,p,pp,m)
+if err != nil {
+  panic(err)
+}
+}
+
+
+func serve(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("main.gohtml","css/main.css","css/mcleod-reset.css"))
+  tpl.Execute(w, nil)
+}
+
+func verify(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("verification.gohtml","css/main.css","css/mcleod-reset.css",))
+  tpl.Execute(w, nil)
+
+}
+
+
+func weeklyregister(w http.ResponseWriter, r *http.Request){
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("tregistration.gohtml","css/main.css","css/mcleod-reset.css",))
+  tpl.Execute(w, nil)
+}
+
+func waitingregister(w http.ResponseWriter, r *http.Request){
+
+  if r.Method == http.MethodPost {
+    email := r.FormValue("email")
+    pass := r.FormValue("pass")
+    ppal := true
+    membf := true
+
+    fmt.Println(email + " signed up with pass:" + pass)
+
+    err := ioutil.WriteFile("test.txt", []byte(email+":"+pass), 0666)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    dbupost(email,pass,ppal,membf)
+
+    http.Redirect(w, r, "/waitingregister", http.StatusSeeOther)
+    }
+
+  var tpl *template.Template
+  tpl = template.Must(template.ParseFiles("waitingverification.gohtml","css/main.css","css/mcleod-reset.css",))
+  tpl.Execute(w, nil)
+
+}
