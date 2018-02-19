2fe84b86a491a98370cf3ec8940335cc362617e2
embulldogs99 <embulldogs99@yahoo.com>
2018-02-19 01:40:03 -0800

    automated posting

--- maintest.go
+++ maintest.go
@@ -121,6 +121,7 @@ func profile(w http.ResponseWriter, r *http.Request){
     var grade int
 
     err = dbusers.QueryRow("SELECT * FROM rfgg.members WHERE email=$1 AND pass=$2",emailcheck,passcheck).Scan(&email, &pass, &ppal, &wins, &losses, &heat, &refers, &memberflag, &credits, &grade)
+    data:="Heat"+heat
     switch{
     case err == sql.ErrNoRows:
       log.Printf("No user with that ID.")
@@ -130,7 +131,7 @@ func profile(w http.ResponseWriter, r *http.Request){
     default:
       var tpl *template.Template
       tpl = template.Must(template.ParseFiles("profile.gohtml","css/main.css","css/mcleod-reset.css",))
-      tpl.Execute(w, heat)
+      tpl.Execute(w, data)
       fmt.Println("success")
       }
   }
