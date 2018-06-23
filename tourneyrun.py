6d0bcccfcae232a8f128cf36b3d45f2dc94a1604
embulldogs99 <embulldogs99@yahoo.com>
2018-06-23 02:14:14 -0700

    update

--- tourneyrun.py
+++ tourneyrun.py
@@ -21,7 +21,7 @@ def statspull(ep):
     solokill=store['stats']['p2']['kills']['valueInt']
     solomatch=store['stats']['p2']['matches']['valueInt']
     kills=squadkill+duokill+solokill
-    curtime=round(time.time()*1000,7)
+    curtime=int(time.time()*100)
     matchcount=squadmatch+duomatch+solomatch
 
     print(' ')
@@ -60,7 +60,7 @@ def playerlist(tournament,rn):
 
 conn = psycopg2.connect("dbname='postgres' user='postgres' password='rk' host='localhost' port='5432'")
 cur = conn.cursor()
-cur.execute("CREATE TABLE rfgg.tourney_temp (epicusername VARCHAR(500),kills INTEGER,matches INTEGER, time_stamp DECIMAL;")
+cur.execute("CREATE TABLE rfgg.tourney_temp (epicusername VARCHAR(500),kills INTEGER,matches INTEGER, time_stamp BIGINT;")
 conn.commit()
 
 for p,rn in playerlist('freeweekly2',1):
