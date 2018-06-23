038455c105149aa4507e990c5fb7bb5d9f4f13fa
embulldogs99 <embulldogs99@yahoo.com>
2018-06-23 02:06:01 -0700

    update

--- tourneyrun.py
+++ tourneyrun.py
@@ -21,7 +21,7 @@ def statspull(ep):
     solokill=store['stats']['p2']['kills']['valueInt']
     solomatch=store['stats']['p2']['matches']['valueInt']
     kills=squadkill+duokill+solokill
-    curtime=int(time.time()*100)
+    curtime=time.time()*100
     matchcount=squadmatch+duomatch+solomatch
 
     print(' ')
