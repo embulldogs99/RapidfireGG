8c2fe2edfb87412c82dabe787fb74e28171fbb95
embulldogs99 <embulldogs99@yahoo.com>
2018-06-24 22:02:07 -0700

    update

--- main.go
+++ main.go
@@ -436,6 +436,8 @@ def playerlist(tournament,rn):
 
 conn = psycopg2.connect("dbname='postgres' user='postgres' password='rk' host='localhost' port='5432'")
 cur = conn.cursor()
+cur.execute("DROP TABLE rfgg.tourney_temp;")
+conn.commit()
 cur.execute("CREATE TABLE rfgg.tourney_temp (epicusername VARCHAR(500),kills INTEGER,matches INTEGER, time_stamp BIGINT);")
 conn.commit()
 
